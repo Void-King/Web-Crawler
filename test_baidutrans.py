fb9f2d0fba911e56e9bd9ca5ca835e4ef50d4dae
Void-King <1402112011@qq.com>
2019-04-23 15:47:35 +0800

    Void-King

--- test_baidutrans.py
+++ test_baidutrans.py
@@ -1,15 +1,35 @@
 import requests
 import json
-url = "https://fanyi.baidu.com/sug"
+
 # 定义请求的参数
 beftrans = 'hello'
-data = {'kw': beftrans}
-# 创建请求， 发送请求， 爬取信息
-res = requests.post(url, data=data)
-# 解析结果
-str_json = res.content.decode("utf-8")
-# print(type(str_json))
-myjson = json.loads(str_json)
-# print(type(myjson))
 print (beftrans)
-print (myjson['data'][0]['v'])
+def apitrans():
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+    }
+    url = "https://fanyi.baidu.com/transapi"# v2transapi fail
+    data = {'from':'en',
+            'to':'zh',
+            'query':beftrans}
+    res = requests.post(url, data=data, headers=headers)
+    struncode = res.content
+    strafcode = json.loads(struncode, encoding='utf8')
+    print (strafcode['data'][0]['dst'])
+def sugtrans():
+    url = "https://fanyi.baidu.com/sug"
+    data = {'kw': beftrans}
+    # 创建请求， 发送请求， 爬取信息
+    res = requests.post(url, data=data)
+    # 解析结果
+    str_json = res.content
+    # print (str_json)
+    # # print(type(str_json))
+    myjson = json.loads(str_json, encoding='utf-8')
+    # print (myjson)
+    # # print(type(myjson))
+    print (myjson['data'][0]['v'])
+
+if __name__ == "__main__":
+    apitrans()
+    sugtrans()
